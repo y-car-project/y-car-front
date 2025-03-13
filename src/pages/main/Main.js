e124871541d8c3154d1d05c52f7cbaeedfa82f2b
udwns310 <uroasecret5@gmail.com>
2025-03-13 15:02:41 +0900

    feat : tailwind 세팅

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,5 +1,5 @@
-import React from "react";
+import React from 'react';
 
 export default function Main() {
-  return <div>Main</div>;
+  return <h1 className="text-4xl font-bold underline text-blue-400">Hello world!</h1>;
 }
