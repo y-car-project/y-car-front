a7cdc9c6d162d6fba24a0e6c22f0defd2d02ffd5
udwns310 <uroasecret5@gmail.com>
2025-03-13 15:13:17 +0900

    연동 테스트

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Main() {
-  return <h1 className="text-4xl font-bold underline text-blue-400">Hello world!</h1>;
+  return <h1 className="text-4xl font-bold underline text-blue-600">Hello world!</h1>;
 }
