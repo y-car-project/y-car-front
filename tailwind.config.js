6990c443509038205fd6a92488f69fbfd591491c
yeeun426 <thsudkcla7@naver.com>
2025-03-18 10:56:25 +0900

    feat : main UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        weekly: 'url(/images/bg-weekly.png)',
+      },
       keyframes: {
         'bounce-timedeal': {
           '0%, 100%': { transform: 'translateY(0)' },
@@ -11,6 +14,9 @@ module.exports = {
       },
       animation: {
         'bounce-timedeal': 'bounce-timedeal 0.7s ease-in-out infinite',
+      },
+      boxShadow: {
+        custom: '0 0.2rem 0.4rem 0 rgba(22, 23, 29, 0.04), 0 0 0.1rem 0 rgba(22, 23, 29, 0.2)',
       },
     },
   },
